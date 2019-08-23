881ba6289bba017bab3a5e5180e60f124ae5357c
Gustavodacrvi <gustavodacrvi@gmail.com>
2019-08-23 12:37:45 -0300

    options.vue

---
+++ src/components/Options.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="card search">
+    <input
+      v-model="search"
+      class="input"
+    />
+  </div>
+</template>
+
+<script lang='ts'>
+
+import { Component, Vue } from 'vue-property-decorator'
+
+@Component
+export default class SearchComp extends Vue {}
+
+</script>
+
+<style scoped>
+
+</style>
